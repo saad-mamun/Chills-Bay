e9d7ab4ab60ca5f1028d5d82d2db05053ed3af78
saad-mamun <mamunali88991@gmail.com>
2024-12-11 14:35:41 +0600

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import 'antd/dist/reset.css'
 import './App.css'
 import React from 'react';
 import Navbar from './Components/Navbar';
-import HeroArea from './Components/HeroArea';
-import Image from './Components/Image';
-import ThingsCard from './Components/ThingsCard';
-import ChillsBay from './Components/ChillsBay';
-import MyCarousel from './Components/Carousel';
-import Gallary from './Components/Gallary';
 import Footer from './Components/Footer';
+import Home from './Components/HomePage';
 
 function App() {
 
@@ -16,12 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <HeroArea />
-      <Image />
-      <ThingsCard />
-      <ChillsBay />
-      <MyCarousel />
-      <Gallary />
+      <Home/>
       <Footer/>
     </>
   )
---
+++ src/Components/HomePage/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HeroArea from './HeroArea'
+import Image from './Image'
+import ThingsCard from './ThingsCard'
+import ChillsBay from './ChillsBay'
+import MyCarousel from './Carousel'
+import Gallary from './Gallary'
+
+function Home() {
+  return (
+    <>
+        <HeroArea/>
+        <Image/>
+        <ThingsCard/>
+        <ChillsBay/>
+        <MyCarousel/>
+        <Gallary/>
+    </>
+  )
+}
+
+export default Home
